a1c3e71fed96ed769ca3a97f322afafdd16811d1
Remon0111 <ren.hashimoto0111@gmail.com>
2025-06-07 13:44:47 +0900

    awsで料金が発生する前に通知する機能を追加しました

---
+++ versions.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 1.5.0, < 2.0.0"
+
+  backend "s3" {
+    bucket = "kdg-aws-2025-hashimotoren"
+    key    = "terraform/state.tfstate"
+    region = "ap-northeast-1"
+    encrypt = true
+    }
+}
